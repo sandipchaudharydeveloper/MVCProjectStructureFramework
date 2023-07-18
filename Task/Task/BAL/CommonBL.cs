d1d84f81a000e76fe1764853fcd2c2b3c1feadb7
Sandip Chaudhary <74008962+sandipchaudharydeveloper@users.noreply.github.com>
2023-07-18 18:46:36 +0530

    initialize code

---
+++ Task/Task/BAL/CommonBL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace Task.BAL
+{
+    public class CommonBL
+    {
+        SqlConnection objSqlConnection;
+        public CommonBL()
+        {
+            ConnectionOpen();
+        }
+        public void ConnectionOpen()
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["yourconnectinstringName"].ConnectionString;
+                objSqlConnection = new SqlConnection(connectionString);
+                objSqlConnection.Open();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void ConnectionClose()
+        {
+            try
+            {
+                objSqlConnection.Close();
+                objSqlConnection.Dispose();
+
+            }
+            catch (Exception)
+            {
+                objSqlConnection.Close();
+                objSqlConnection.Dispose();
+                throw;
+            }
+            finally
+            {
+                objSqlConnection.Close();
+                objSqlConnection.Dispose();
+            }
+        }
+    }
+}
